b768c7a7ace028d01069b42ea259cb2d504a432b
Duc Pham <inducpham@gmail.com>
2020-04-08 21:28:31 +0700

    + Add UICameraScaler

---
+++ UICameraScaler/UICameraScaler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, RequireComponent(typeof(Camera))]
+public class UICameraScaler : MonoBehaviour
+{
+    public Camera targetCamera;
+    public Vector2Int screenReferenceSize;
+    public float baseCameraSize = 8;
+
+    private void Reset()
+    {
+        this.targetCamera = this.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.screenReferenceSize.x <= 0 || this.screenReferenceSize.y <= 0)
+            return;
+
+        var screen_ratio = Screen.width * 1.0f / Screen.height;
+        var ratio = this.screenReferenceSize.x * 1.0f / this.screenReferenceSize.y;
+
+        if (ratio < screen_ratio)
+            this.targetCamera.orthographicSize = this.baseCameraSize;
+        else
+            this.targetCamera.orthographicSize = this.baseCameraSize * ratio / screen_ratio;
+
+    }
+}
