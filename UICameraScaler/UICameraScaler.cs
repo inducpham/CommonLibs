90d6dd549c640da6365596fb0bb903083658cdf7
Duc Pham <inducpham@gmail.com>
2020-08-10 02:32:41 +0700

    - fix camera scaler issue

--- UICameraScaler/UICameraScaler.cs
+++ UICameraScaler/UICameraScaler.cs
@@ -14,6 +14,11 @@ public class UICameraScaler : MonoBehaviour
         this.targetCamera = this.GetComponent<Camera>();
     }
 
+    private void Awake()
+    {
+        Update();
+    }
+
     // Update is called once per frame
     void Update()
     {
